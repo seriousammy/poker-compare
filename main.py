63bf0b0338c01c2ff579002f58c13f8535e53adc
sam_l <sam019@hotmail.com>
2018-10-10 11:22:22 -0700

    Added poker game logic in main, and testing with a single hand.

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 from poker.card import Card
 from poker.deck import Deck
 from poker.card_map import CARD_VALUES, CARD_SUITS
+from poker.poker import Poker
+from poker.hand import Hand
 
 
 def main():
     print('Welcome to Poker Scorer')
     card_array = [Card(value, suit) for suit in CARD_SUITS for value in CARD_VALUES]
     deck = Deck(card_array)
+    poker_game = Poker(deck)
+    hand = Hand([Card('3', 'hearts'), Card('4', 'hearts'), Card('5', 'diamonds'), Card('4', 'hearts'), Card('2', 'hearts')])
+    poker_game.add_single_hand_to_game(hand)
+    poker_game.get_score()
+
 
 if __name__ == '__main__':
     main()
--- poker/deck.py
+++ poker/deck.py
@@ -3,6 +3,6 @@ class Deck:
 
     def __init__(self, cards):
         self.deck = cards
-    
+
     def __str__(self):
         return str(self.deck)
