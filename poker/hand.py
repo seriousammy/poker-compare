e6366de615fa18157a4f74ff630aae82c053d2e2
sam_l <sam019@hotmail.com>
2018-10-10 11:41:45 -0700

    Added scoring that I missed pertaining to value logic, 3 of a kind, 4 of a kind, and added flush scoring using similar value logic but using the suit attribute of the card.

--- poker/hand.py
+++ poker/hand.py
@@ -13,30 +13,68 @@ class Hand:
                 card_dict.update({card.value: 1})
         return card_dict
 
+    def convert_card_to_suit_dict(self):
+        card_dict = {}
+        for card in self.hand:
+            try:
+                card_dict[card.suit] += 1
+            except KeyError:
+                card_dict.update({card.suit: 1})
+        return card_dict
+
+    def is_four_of_a_kind(self):
+        card_value_dict = self.convert_card_to_value_dict()
+        if sorted(list(card_value_dict.values())) == [1, 4]:
+            return True
+        return False
+
+    def is_three_of_a_kind(self):
+        card_value_dict = self.convert_card_to_value_dict()
+        if sorted(list(card_value_dict.values())) == [1, 1, 3]:
+            return True
+        return False
+
     def is_pair(self):
-        card_dict = self.convert_card_to_value_dict()
-        if sorted(list(card_dict.values())) == [1, 1, 1, 2]:
+        card_value_dict = self.convert_card_to_value_dict()
+        if sorted(list(card_value_dict.values())) == [1, 1, 1, 2]:
             return True
         return False
 
     def is_two_pair(self):
-        card_dict = self.convert_card_to_value_dict()
-        if sorted(list(card_dict.values())) == [1, 2, 2]:
+        card_value_dict = self.convert_card_to_value_dict()
+        if sorted(list(card_value_dict.values())) == [1, 2, 2]:
             return True
         return False
 
     def is_full_house(self):
-        card_dict = self.convert_card_to_value_dict()
-        if sorted(list(card_dict.values())) == [2, 3]:
+        card_value_dict = self.convert_card_to_value_dict()
+        if sorted(list(card_value_dict.values())) == [2, 3]:
+            return True
+        return False
+
+    def is_flush(self):
+        card_suit_dict = self.convert_card_to_suit_dict()
+        if len(card_suit_dict) == 1:
             return True
         return False
 
     def get_score(self):
-        if self.is_pair():
-            return "1 Pair"
-        elif self.is_two_pair():
-            return "2 Pair"
+        # TODO: Royal Flush
+        # TODO: Straight Flush
+        if self.is_four_of_a_kind():
+            return "Four of a Kind"
         elif self.is_full_house():
             return "Full House"
+        elif self.is_flush():
+            return "Flush"
+        # TODO: Straight
+        elif self.is_three_of_a_kind():
+            return "3 of a Kind"
+        elif self.is_two_pair():
+            return "2 Pair"
+        elif self.is_pair():
+            return "1 Pair"
+        elif self.is_flush():
+            return "Flush"
         else:
-            return "No Score"
+            return "High Card"
--- tests/test_hand.py
+++ tests/test_hand.py
@@ -5,7 +5,7 @@ from poker.hand import Hand
 def test_hand_initialize():
     hand = Hand([Card('3', 'hearts'), Card('6', 'hearts'), Card('5', 'diamonds'), Card('4', 'hearts'), Card('2', 'hearts')])
     hand_score = hand.get_score()
-    assert hand_score == 'No Score'
+    assert hand_score == 'High Card'
 
 
 def test_one_pair_hand():
@@ -18,3 +18,21 @@ def test_two_pair_hand():
     hand = Hand([Card('3', 'hearts'), Card('4', 'diamonds'), Card('2', 'diamonds'), Card('4', 'hearts'), Card('2', 'hearts')])
     hand_score = hand.get_score()
     assert hand_score == '2 Pair'
+
+
+def test_three_of_a_kind():
+    hand = Hand([Card('3', 'hearts'), Card('4', 'diamonds'), Card('4', 'spades'), Card('4', 'hearts'), Card('2', 'hearts')])
+    hand_score = hand.get_score()
+    assert hand_score == '3 of a Kind'
+
+
+def test_flush():
+    hand = Hand([Card('3', 'hearts'), Card('4', 'hearts'), Card('2', 'hearts'), Card('7', 'hearts'), Card('8', 'hearts')])
+    hand_score = hand.get_score()
+    assert hand_score == 'Flush'
+
+
+def test_four_of_a_kind():
+    hand = Hand([Card('3', 'hearts'), Card('3', 'diamonds'), Card('3', 'clubs'), Card('3', 'spades'), Card('2', 'hearts')])
+    hand_score = hand.get_score()
+    assert hand_score == 'Four of a Kind'
