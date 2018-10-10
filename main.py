64a77fe0f963366299891acc393e60b177aa04a4
sam_l <sam019@hotmail.com>
2018-10-10 16:11:00 -0700

    Altered main.py to have functions to reflect comparing hands in the game.

--- main.py
+++ main.py
@@ -10,9 +10,12 @@ def main():
     card_array = [Card(value, suit) for suit in CARD_SUITS for value in CARD_VALUES]
     deck = Deck(card_array)
     poker_game = Poker(deck)
-    hand = Hand([Card('3', 'hearts'), Card('4', 'hearts'), Card('5', 'diamonds'), Card('4', 'hearts'), Card('2', 'hearts')])
-    poker_game.add_single_hand_to_game(hand)
-    poker_game.get_score()
+    hand_1 = Hand([Card('3', 'hearts'), Card('4', 'hearts'), Card('5', 'diamonds'), Card('4', 'clubs'), Card('2', 'hearts')])
+    hand_2 = Hand([Card('2', 'spades'), Card('4', 'spades'), Card('5', 'diamonds'), Card('4', 'diamonds'), Card('2', 'hearts')])
+
+    poker_game.add_single_hand_to_game(hand_1)
+    poker_game.add_single_hand_to_game(hand_2)
+    print(poker_game.compare_two_hands())
 
 
 if __name__ == '__main__':
