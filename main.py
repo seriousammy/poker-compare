9a1a255221ea27e6e699f5d740741967bb9e7c91
sam_l <sam019@hotmail.com>
2018-10-10 09:50:52 -0700

    Addition of main and card_map, the purpose of card map is to contain the possible cards in a deck.

---
+++ main.py
@@ -0,0 +1,5 @@
+def main():
+    print('Welcome to Poker Scorer')
+
+if __name__ == '__main__':
+    main()
---
+++ poker/card_map.py
@@ -0,0 +1,2 @@
+CARD_VALUES = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+CARD_SUITS = ['spades', 'hearts', 'clubs', 'diamonds']
