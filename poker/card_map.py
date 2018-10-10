0fd4452879a89f3a4ce13d3f598bf41006e1e40b
sam_l <sam019@hotmail.com>
2018-10-10 15:18:09 -0700

    Added map for Hand Rank and Card Suit scoring maps. This is rank of the hand and suits respectively stored in a map.

--- poker/card_map.py
+++ poker/card_map.py
@@ -15,3 +15,51 @@ CARD_VALUE_SCORING_MAP = {
     'K': 13,
     'A': 14
 }
+CARD_SUITS_SCORING_MAP = {
+    'diamonds': 0,
+    'clubs': 1,
+    'hearts': 2,
+    'spades': 3
+}
+CARD_VALUE_RANK = {
+    'high_card': {
+        'value': 0,
+        'name': 'High Card'
+    },
+    'one_pair': {
+        'value': 1,
+        'name': '1 Pair'
+    },
+    'two_pair': {
+        'value': 2,
+        'name': '2 Pair'
+    },
+    'three_of_a_kind': {
+        'value': 3,
+        'name': '3 of a Kind'
+    },
+    'straight': {
+        'value': 4,
+        'name': 'Straight'
+    },
+    'flush': {
+        'value': 5,
+        'name': 'Flush'
+    },
+    'full_house': {
+        'value': 6,
+        'name': 'Full House'
+    },
+    'four_of_a_kind': {
+        'value': 7,
+        'name': 'Four of a Kind'
+    },
+    'straight_flush': {
+        'value': 8,
+        'name': 'Straight Flush'
+    },
+    'royal_flush': {
+        'value': 9,
+        'name': 'Royal Flush'
+    }
+}
