8d3b68453304656e0d66d09d73f663fcc8801df9
sam_l <sam019@hotmail.com>
2018-10-10 10:11:25 -0700

    Initialization of a deck of cards in main, this will be used to inject a deck into the class of Poker (to be introduced).

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+from poker.card import Card
+from poker.deck import Deck
+from poker.card_map import CARD_VALUES, CARD_SUITS
+
+
 def main():
     print('Welcome to Poker Scorer')
+    card_array = [Card(value, suit) for suit in CARD_SUITS for value in CARD_VALUES]
+    deck = Deck(card_array)
 
 if __name__ == '__main__':
     main()
